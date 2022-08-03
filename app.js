4646e712ac26f76a20397a960646974c9566edb2
Hardik Desai <hp676913@gmail.com>
2022-08-03 12:45:03 +0530

    All bugs fix - make responsive

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let wheel = document.getElementById('wheel');
 let arrow = document.getElementById('arrow');
-let btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
+let btnSpin = document.getElementById('btnSpin');
+let btnReset = document.getElementById('btnReset');
+function spinWheel() {
     let num = Math.floor(720 + (1800 - 720) * Math.random());
     console.log(num);
     wheel.style.transitionProperty = "all";
     wheel.style.transitionDuration = "7s";
     wheel.style.transitionTimingFunction = "ease";
     wheel.style.transform = `rotate(${num}deg)`;
-});
+    btnReset.style.display = 'block';
+    btnSpin.style.display = 'none';
+}
+
